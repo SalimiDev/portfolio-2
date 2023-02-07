25d8c789059bd05d18d96ad6c05dcd74fc3bfa0b
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-07 14:22:23 +0330

    fixed error in Banner component

--- src/components/Bannner.js
+++ src/components/Bannner.js
@@ -14,7 +14,7 @@ const Bannner = () => {
     const [text, setText] = useState('');
     const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer'];
     const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [setIndex] = useState(1);
+    const [index, setIndex] = useState(1);
     const period = 2000;
 
     useEffect(() => {
